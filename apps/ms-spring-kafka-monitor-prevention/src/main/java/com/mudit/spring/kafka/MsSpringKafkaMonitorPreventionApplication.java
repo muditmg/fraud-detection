649453cd3f768f7f480d8f2add340a22026a0380
muditmg <mudit.gupta02100@gmail.com>
2025-02-27 14:08:15 +0530

    added functionalities

--- apps/ms-spring-kafka-monitor-prevention/src/main/java/com/mudit/spring/kafka/MsSpringKafkaMonitorPreventionApplication.java
+++ apps/ms-spring-kafka-monitor-prevention/src/main/java/com/mudit/spring/kafka/MsSpringKafkaMonitorPreventionApplication.java
@@ -2,12 +2,12 @@ package com.zomeli.spring.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zomeli.spring.kafka", "com.zomeli.spring.kafka.controller"})
 public class MsSpringKafkaMonitorPreventionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsSpringKafkaMonitorPreventionApplication.class, args);
 	}
-
 }
--- apps/ms-spring-kafka-streams-monitor-prevention/src/main/java/com/mudit/spring/kafka/streams/MsSpringKafkaStreamsMonitorPreventionApplication.java
+++ apps/ms-spring-kafka-streams-monitor-prevention/src/main/java/com/mudit/spring/kafka/streams/MsSpringKafkaStreamsMonitorPreventionApplication.java
@@ -2,12 +2,12 @@ package com.zomeli.spring.kafka.streams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zomeli.spring.kafka.streams", "com.zomeli.spring.kafka.controller"})
 public class MsSpringKafkaStreamsMonitorPreventionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsSpringKafkaStreamsMonitorPreventionApplication.class, args);
 	}
-
 }
